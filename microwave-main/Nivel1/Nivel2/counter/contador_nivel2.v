b995fb49a15c137a219b9418305b079292f0f47f
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-04-24 14:59:45 -0300

    Update contador_nivel2.v

--- microwave-main/Nivel1/Nivel2/counter/contador_nivel2.v
+++ microwave-main/Nivel1/Nivel2/counter/contador_nivel2.v
@@ -3,13 +3,14 @@
 
 `timescale 1ns/1ps
 
-module contador_nivel2(uni_sec, dez_sec, min, clk, enable, load, count_us, count_ds, count_m, zero);
+module contador_nivel2(uni_sec, clear, clk, enable, load, count_us, count_ds, count_m, zero);
 
-  input [3:0] uni_sec, dez_sec, min;
+  input [3:0] uni_sec;
   input wire clk, enable, load, clear;
   output wire [3:0] count_us, count_ds, count_m;
   output wire zero;
   
+  wire [3:0] dez_sec, min;
   wire tc1;
   wire tc2;
   wire tc4;
